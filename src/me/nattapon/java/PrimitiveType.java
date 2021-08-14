af0d802f4364c6df89e763f7e9e2d2799205ba38
nuinattapon <nattapon.subanake@gmail.com>
2021-08-14 16:20:21 +0700

    First commit

---
+++ src/me/nattapon/java/PrimitiveType.java
@@ -0,0 +1,22 @@
+package me.nattapon.java;
+
+public class PrimitiveType {
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 1;
+        int i = 2;
+        long l = 3;
+        float f = 4;
+        double d = 5;
+        char c = 'c';
+        boolean bool = true;
+
+        System.out.printf("byte %d\n",b);
+        System.out.printf("short %d\n",s);
+        System.out.printf("int %d\n",i);
+        System.out.printf("long %d\n",l);
+        System.out.printf("float %.0f\n",f);
+        System.out.printf("double %.0f\n",d);
+        System.out.printf("char '%s'\n",c);
+        System.out.printf("boolean %b\n",bool);    }
+}
